b85618cfa48551e21fc66a122e6c95e5af2bcadb
Kalpon Gilop <bridgez712@gmail.com>
2020-12-16 21:17:21 +0700

    4)Определите функционал меню таким образом, чтобы при выборе: a.)«ButtonClick» - интерфейс соответствовал рис.1 (одно текстовое поле и кнопка) и при нажатии на кнопку появлялось сообщение «Button clicked», а при длительном нажатии на кнопку появлялось сообщение «Long button click»

--- app/src/main/java/com/example/a/eventhandling/MainActivity.java
+++ app/src/main/java/com/example/a/eventhandling/MainActivity.java
@@ -3,17 +3,39 @@ package com.example.a.eventhandling;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonClick;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        buttonClick = findViewById(R.id.buttonClick);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 textView.setText("ButtonClick");
+             }
+         });
+        buttonClick.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                textView.setText("LongButtonClick");
+                return false;
+            }
+        });
     }
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
 }
